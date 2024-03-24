28804ec73777185882b262e03c12614e90ca70a7
Shreeram <deepskinweb@gmail.com>
2024-03-24 17:05:06 +0530

    Date 24 Mar 2024

--- src/components/Brand/BrandLogin.js
+++ src/components/Brand/BrandLogin.js
@@ -10,6 +10,8 @@ import { useDispatch} from 'react-redux';
 import {login} from '../../store/brandSlice';
 import { useSelector } from "react-redux";
 import CircularProgress from '@mui/material/CircularProgress';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 
 
@@ -20,6 +22,8 @@ function BrandLogin() {
   const [password, getPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const user = useSelector((state) => state.brandUser);
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   // const baseUrl = "http://localhost:8000/api";
 
   
@@ -105,6 +109,103 @@ function BrandLogin() {
 
   return (
     <>
+
+{isSmallScreen ? (
+
+<Grid item xs={12} paddingX ={2}>
+
+<form action="#" method="post">
+
+{isLoading ? (
+ <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%', marginTop: '30%' }}>
+ <CircularProgress color= 'success' />
+</div>
+) : ( <>
+  <Box
+    display="flex"
+    flexDirection={"column"}
+    maxWidth={450}
+    margin="auto"
+    marginTop={15}
+    padding={1}
+  >
+    <Typography variant="h5" padding={3} textAlign="center">
+      Brand Login
+    </Typography>
+
+    <TextField
+      type="email"
+      id="email"
+      onChange={(e) => {
+        getEmail(e.target.value);
+      }}
+      margin="normal"
+      variant="outlined"
+      label="Work Email"
+    ></TextField>
+    <TextField
+      type="password"
+      id="password"
+      onChange={(e) => {
+        getPassword(e.target.value);
+      }}
+      margin="normal"
+      variant="outlined"
+      label="Enter Password"
+    ></TextField>
+
+      <Typography variant="body2" sx={{display: 'flex', justifyContent: 'flex-end'}}>
+      <Link href="/forgotPassword" underline="none" sx={{ color: '#362FD9'}}>
+      Forgot password?
+      </Link>
+    </Typography>
+
+    <Button
+      type="submit"
+      onClick={handleSubmit}
+      variant="contained"
+      sx={{
+        marginTop: 2,
+        textTransform: "capitalize",
+        fontWeight: "300",
+        fontSize: 16,
+        background: '#362FD9'
+      }}
+      size="large"
+    >
+      Login
+    </Button>
+    <ToastContainer autoClose= {2000}/>
+
+
+    <Typography variant="body2" sx={{marginTop : '5px'}}>
+      I agree to{" "}
+      <Link href="https://www.broadreach.in/terms-conditions" target="_blank" underline="none" sx={{color: '#362FD9'}}>
+        BroadReach's Terms of Service
+      </Link>
+    </Typography>
+
+    <Button
+      variant="outlined"
+      size="large"
+      sx={{
+        marginTop: 3,
+        textTransform: "capitalize",
+        fontWeight: "400",
+        fontSize: 16,
+        color: '#362FD9'
+      }}
+      onClick={signupButton}
+    >
+      Create new Account Here
+    </Button>
+  </Box>
+  </> )}
+</form>
+
+</Grid> ): (
+
+
       <Grid container spacing="1" sx={{ height: '100vh' }}>
 
       <Grid item xs={4} sx={{ background: '#362FD9' }}>
@@ -260,7 +361,7 @@ function BrandLogin() {
 
         
 
-      </Grid>
+      </Grid>)}
     </>
   );
 }
