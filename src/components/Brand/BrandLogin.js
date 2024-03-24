93c8243a4f6dac5589b938b7f21db7a73f2f4b63
Shreeram <deepskinweb@gmail.com>
2024-03-24 17:18:40 +0530

    Date 24 Mar 2024

--- src/components/Brand/BrandLogin.js
+++ src/components/Brand/BrandLogin.js
@@ -61,7 +61,7 @@ function BrandLogin() {
       setIsLoading(true);
 
       
-      await axios.post("/brand/brand-login",
+      await axios.post("/api/brand/brand-login",
         { email: email.toLowerCase(), password: password },
         {withCredentials: true}
       )
