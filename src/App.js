d57f9b3af24390d9b7161e98cf9a4013f47636a6
Shreeram <deepskinweb@gmail.com>
2024-03-26 12:15:23 +0530

    Date 26 Mar 2024

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
     <Provider store={store}>
     <div className="App">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&family=Poppins:wght@300;400;500;600;700;800&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
 
       <Router>
